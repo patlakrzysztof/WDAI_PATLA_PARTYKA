d51f04abf180979558dea154edc2c80ab8944971
patlakrzysztof <patlakrzysztof1@gmail.com>
2025-12-24 10:22:41 +0100

    Material UI works

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,8 +1,19 @@
+import { Button, Card, CardContent, Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-red-500">Tailwind działa 🔥</div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <Card className="p-6">
+        <CardContent className="flex flex-col gap-4 items-center">
+          <Typography variant="h5">MUI + Tailwind working</Typography>
+
+          <Button variant="contained" color="primary">
+            Works
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
