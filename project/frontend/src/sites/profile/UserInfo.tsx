0fe65d7cadfe8a90a7335720a9993881b21794fc
Mateusz <187099271+5moothie@users.noreply.github.com>
2026-01-09 09:37:02 +0100

    modified User info to look better

--- project/frontend/src/sites/profile/UserInfo.tsx
+++ project/frontend/src/sites/profile/UserInfo.tsx
@@ -16,14 +16,6 @@ const user: User = {
   id: 124444,
   mail: "someMail@gmail.com",
   phone: "48 695 209 243",
-  address: {
-    country: "Poland",
-    city: "Krakow",
-    zipCode: "20-231",
-    street: "Imaginary",
-    houseNumber: 20,
-    flatNumber: 3,
-  },
 };
 
 export default function UserInfo() {
@@ -34,66 +26,54 @@ export default function UserInfo() {
   };
 
   return (
-    <Card variant="outlined" className="w-full max-w-[20rem]">
-      <CardContent>
-        <div className="mb-4">
-          <Typography variant="h5" className="black">
-            {" "}
-            {user.name + " " + user.surname}
+    <Card
+      elevation={5}
+      className="w-full max-w-[400px] bg-white rounded-lg overflow-visible relative mt-10 px-4 py-5"
+    >
+      <CardContent className="pt-12 px-8">
+        <div className="flex flex-col items-center mb-6">
+          <Typography variant="h5" color="primary" fontWeight="bold">
+            {user.name} {user.surname}
           </Typography>
-          <Typography variant="body1">{user.nickname}</Typography>
-          <Typography variant="body2">
-            {isShown ? `#${user.id}` : "#******"}
+          <Typography variant="subtitle1" color="secondary">
+            @{user.nickname}
           </Typography>
         </div>
 
         <Divider />
 
-        <div className="my-4">
-          {isShown ? (
-            <>
-              <Typography variant="body1">e-mail: {user.mail}</Typography>
-              <Typography variant="body1">
-                phone: {user.phone ? user.phone : "---"}
+        <div className="flex flex-col gap-3 mt-4">
+          <div className="flex justify-between">
+            <Typography color="secondary">ID:</Typography>
+            <Typography fontWeight="medium">
+              #{isShown ? user.id : "●●●●●●"}
+            </Typography>
+          </div>
+          <div className="flex justify-between">
+            <Typography color="secondary">Email:</Typography>
+            <Typography fontWeight="medium">
+              {isShown ? user.mail : "●●●●●●@gmail.com"}
+            </Typography>
+          </div>
+          <div className="flex justify-between">
+            <Typography color="secondary">Phone:</Typography>
+            {isShown ? (
+              <Typography fontWeight="medium">
+                +{user.phone ? user.phone : "-- --- ---"}
               </Typography>
-            </>
-          ) : (
-            <>
-              <Typography variant="body1">e-mail: ****</Typography>
-              <Typography variant="body1">phone: ****</Typography>
-            </>
-          )}
-        </div>
-
-        <Divider />
-
-        <div className="my-4">
-          <Typography variant="subtitle2" gutterBottom>
-            Delivery address:
-          </Typography>
-          {isShown ? (
-            user.address ? (
-              <>
-                <Typography variant="body1">{user.address.country}</Typography>
-                <Typography variant="body1">{user.address.city}</Typography>
-                <Typography variant="body1">
-                  {user.address.street} st. {user.address.houseNumber}
-                  {user.address.flatNumber ? "/" + user.address.flatNumber : ""}
-                </Typography>
-                <Typography variant="body1">{user.address.zipCode}</Typography>
-                <Typography variant="body1">{user.address.city}</Typography>
-              </>
             ) : (
-              <Typography variant="body1">---</Typography>
-            )
-          ) : (
-            <Typography variant="body1">****</Typography>
-          )}
+              <Typography fontWeight="medium">+●● ●●● ●●● ●●●</Typography>
+            )}
+          </div>
         </div>
       </CardContent>
-      <CardActions>
-        <Button size="small" onClick={showHideData}>
-          {isShown ? "hide" : "show"}
+      <CardActions className="justify-center pb-6">
+        <Button
+          variant="contained"
+          onClick={showHideData}
+          className="bg-primary hover:bg-[#1A2D27] normal-case px-8"
+        >
+          {isShown ? "Hide Details" : "Show Details"}
         </Button>
       </CardActions>
     </Card>
--- project/frontend/src/sites/profile/UserPage.tsx
+++ project/frontend/src/sites/profile/UserPage.tsx
@@ -1,10 +1,17 @@
 import UserInfo from "./UserInfo";
+import UserOrders from "./UserOrders";
 
 export default function UserPage() {
   return (
-    <div className="w-full flex flex-row items-center justify-around bg-primary p-10 min-h-[calc(100vh-65px)]">
+    <div
+      className="w-full 
+    flex flex-col items-center justify-evenly 
+    bg-background p-10 
+    min-h-[calc(100vh-65px)] gap-10
+    lg:flex-row lg:items-start"
+    >
       <UserInfo />
-      {/* <UserOrders /> */}
+      <UserOrders />
     </div>
   );
 }
