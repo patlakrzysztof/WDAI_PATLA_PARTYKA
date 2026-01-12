367ccc674e7f76808aab69af62033322ff58df37
Mateusz <187099271+5moothie@users.noreply.github.com>
2026-01-12 12:18:55 +0100

    changed the name of one file

---
+++ project/frontend/src/sites/profile/ProfilePage.tsx
@@ -0,0 +1,27 @@
+import UserPage from "./UserPage";
+import NotLoggedInPage from "./NotLoggedInPage";
+import { useEffect, useState } from "react";
+
+function ProfilePage() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkUserStatus = async () => {
+      try {
+        const res = await fetch("http://localhost:3002/users/me");
+        if (res.ok) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    checkUserStatus();
+  }, []);
+
+  return <>{isLoggedIn ? <UserPage /> : <NotLoggedInPage />}</>;
+}
+
+export default ProfilePage;
