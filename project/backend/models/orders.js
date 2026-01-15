9b446e6f3091c59e32c2506c71d6fcb608f08208
Mateusz <187099271+5moothie@users.noreply.github.com>
2026-01-15 20:55:29 +0100

    added Orders model

---
+++ project/backend/models/orders.js
@@ -0,0 +1,74 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database");
+
+const Orders = sequelize.define(
+  "Orders",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    orderDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    sentDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    inDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    items: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      validate: {
+        structureValidator(value) {
+          if (!Array.isArray(value)) {
+            throw new Error("Items must be an array");
+          }
+          for (const item of value) {
+            if (
+              !item.productId ||
+              !item.productName ||
+              !item.quantity ||
+              !item.priceAtPurchase
+            ) {
+              throw new Error("Each item must have a productId and quantity");
+            }
+          }
+        },
+      },
+    },
+    address: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      validate: {
+        structureValidator(value) {
+          if (
+            !value.country ||
+            !value.city ||
+            !value.zipCode ||
+            !value.street ||
+            !value.houseNumber
+          ) {
+            throw new Error(
+              "Address must contain street, city, postalCode and houseNumber"
+            );
+          }
+        },
+      },
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = Orders;
